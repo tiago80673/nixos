22f35f9e734d2d239c67e79068b14769a1db4076
Tiago Caixinha <tiago.caixinha@tecnico.ulisboa.pt>
2025-09-01 23:43:35 +0100

    simple firefox working config w extensions and custom css

--- modules/workstation/firefox.nix
+++ modules/workstation/firefox.nix
@@ -1,12 +1,24 @@
 {
+	pkgs,
 	...
 }: {
 	programs.firefox = {
-		# enable = true; # need to manage users because this will create a new one (i think)
-		profiles."tc" = {
+		enable = true; # note that this will create a new firefox profile on .mozilla folder with the name bellow. Can check profiles on about:profiles
+		profiles."tiagocaixinha" = { # just be sure that a tiagocaixinha folder doesn't already exist under .mozzila/firefox
 			settings = {
-				"browser.download.open_pdf_attachments_inline" = true;
+				"browser.download.open_pdf_attachments_inline" = true; # avoid downloading
+				"toolkit.legacyUserProfileCustomizations.stylesheets" = true; # allow custom css
 			};
+			userChrome = ''
+				/* Only show close button on the active tab */
+				.tabbrowser-tab:not([selected]) .tab-close-button {
+					visibility: collapse !important;
+				}
+			'';
+			  extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+				tridactyl
+				bitwarden
+			];
 		};
 	};
 }
