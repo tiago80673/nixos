3be8be68eb44fe36be31e4a1c5911bc00f2f8392
Tiago Caixinha <tiago.caixinha@tecnico.ulisboa.pt>
2025-11-11 15:08:57 +0100

    nixvim: added keybinds to cycle buffers and tabbing

--- modules/workstation/nixvim/keymaps.nix
+++ modules/workstation/nixvim/keymaps.nix
@@ -1,3 +1,46 @@
 {
-	
+	programs.nixvim.keymaps = [
+		{
+		  key = "<leader>df";
+		  action = "<cmd>lua vim.lsp.buf.code_action()<cr>";
+		  options = {
+			silent = true;
+			desc = "Code action";
+		  };
+		}
+		{
+		  key = "<tab>";
+		  action = "<cmd>bnext<cr>";
+		  options = {
+			silent = true;
+			desc = "Next buffer";
+		  };
+		}
+		{
+		  key = "<S-tab>";
+		  action = "<cmd>bprevious<cr>";
+		  options = {
+			silent = true;
+			desc = "Previous buffer";
+		  };
+		}
+		{
+		  mode = "v";
+		  key = "<Tab>";
+		  action = ">gv";
+		  options = {
+			desc = "Increase indent and reselect";
+			silent = true;
+		  };
+		}
+		{
+		  mode = "v";
+		  key = "<S-Tab>";
+		  action = "<gv";
+		  options = {
+			desc = "Decrease indent and reselect";
+			silent = true;
+		  };
+		}
+	];	
 }
