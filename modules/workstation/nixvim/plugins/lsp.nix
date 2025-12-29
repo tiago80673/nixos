7c64ac3fe7096ca496ba9466895f6b3162f7f012
Tiago Caixinha <tiago.caixinha@tecnico.ulisboa.pt>
2025-12-29 15:48:44 +0100

    nixvim: add clangd engine selection at runtime

--- modules/workstation/nixvim/plugins/lsp.nix
+++ modules/workstation/nixvim/plugins/lsp.nix
@@ -26,19 +26,31 @@
 	      };
 	    };
 
+		# decide which clangd engine to use from CLANGD_BIN
+		# allows to change which clangd nvim uses to match the current c/cpp environment
+		# I think either lsp.nvim or nixvim should retrieve this from which clangd resolves first in PATH instead of this hack
+		# should it be on luaConfig.post?
+		# prints on_attach / on every new buffer, feels like too much
+		luaConfig.post = ''
+  local clangd = vim.env.CLANGD_BIN
+
+  if not clangd or clangd == "" then
+	  vim.notify("CLANGD_BIN not set; using default clangd", vim.log.levels.WARN)
+  else
+	  vim.lsp.config["clangd"] = {
+		  cmd = { clangd, "--background-index" },
+		  on_attach = function(_, _)
+			  vim.notify("clangd attached: " .. clangd)
+		  end,
+	  }
+  end
+  '';
 
 	servers = {
 		bashls.enable = true;
 		
 		clangd = {
 			enable = true;
-			# TODO: one day make this work, but not here
-			# cmd =[
-				# "clangd"
-				# "--query-driver=/nix/store/y7qcrqigrbxivnm7lzav8fqy0mx44918-xtensa-esp-elf-esp-idf-v5.5.1/bin/xtensa-esp32-elf-gcc"
-				# "--extra-arg=-I${pkgs.esp-idf-full}/components/linux/include"
-				# "--extra-arg=-I${pkgs.esp-idf-full}/components/newlib/platform_include"
-			# ];
 		};
 
 		texlab.enable = true; #latex
